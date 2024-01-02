0276581921c787207b4b6e55742c644e665c5b94
abdulrahman9901 <aba884682@gmail.com>
2024-01-02 11:19:42 +0200

    Server crash issue

--- proxy/index.js
+++ proxy/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.post('/process-image', async (req, res) => {
   try {
     const imageUrl = req.body.url;
-
+    console.log('Error processing image:', error);
     // Fetch the image from the provided URL
     const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
     const imageData = Buffer.from(response.data, 'binary');
@@ -26,6 +26,7 @@ app.post('/process-image', async (req, res) => {
     res.writeHead(200, { 'Content-Type': 'image/png' });
     res.end(processedImage, 'binary');
   } catch (error) {
+    console.log('Error processing image:', error);
     console.error('Error processing image:', error);
     res.status(500).send('Internal Server Error');
   }
@@ -36,5 +37,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port :${port}`);
 });
